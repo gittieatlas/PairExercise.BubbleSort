0342a74d5d31125d97df8eda27a109d65913d921
Dan Gilbert <Daniel.gilbert0051@gmail.com>
2018-07-30 14:57:43 -0400

    initial commit

---
+++ bubblesort.js
@@ -0,0 +1,35 @@
+function bubbleSort(array) {
+  let sortedArr = [];
+
+  // looping:
+  // take two and swap or not
+  // when reaches the end
+  // sortedArray.unshift(array.pop())
+
+  while (array.length) {
+    if (array.length === 1) {
+      sortedArr.unshift(array.pop());
+      break;
+    }
+
+    for (let i = 0; i < array.length - 1; i++) {
+      const leftElement = array[i];
+      const rightElement = array[i + 1];
+
+      if (rightElement > leftElement) {
+        let obj = swap(leftElement, rightElement);
+        array[i] = obj.right;
+        array[i + 1] = obj.left;
+      }
+    }
+  }
+
+  return sortedArr;
+}
+
+function swap(left, right) {
+  return {
+    left: left < right ? left : right,
+    right: left > right ? left : right
+  };
+}
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,1 +1,18 @@
+describe('Bubble Sort', function() {
+  spyOn(bubbleSort, 'swap').and.callThrough();
 
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+
+  it('handles an single item in array', function() {
+    expect(bubbleSort([1])).toEqual([1]);
+  });
+
+  it('handles an multiple items in array', function() {
+    expect(bubbleSort([1, 3, 2])).toEqual([1, 2, 3]);
+  });
+
+  // TODO not use sort method
+  // TODO check if using bubble sort algorithm
+});
